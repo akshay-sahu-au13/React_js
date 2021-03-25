25e9846039abe53ead140d6311f151fec2a202bd
Akshay Sahu <akki5233@gmail.com>
2021-03-25 21:14:26 +0530

    Setting up react components

--- React_basics/first_react_15Mar/my_first_app/src/index.js
+++ React_basics/first_react_15Mar/my_first_app/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import ReactDom from 'react-dom';
+
+// stateless functional component
+// always return JSX
 
 function Greeting() {
-  return <h4>This is Akshay and this is my first react app</h4>
+  return <h4> Hello, Akshay here, and this is my first react app </h4>;
 }
+
+ReactDom.render(<Greeting />  , document.getElementById('root'));
