be7dff6cc7c13472303d7771c190f5e7d4b15b96
Akshay Sahu <akki5233@gmail.com>
2021-03-25 21:52:46 +0530

    Creating react components and using it inside another components

--- React_basics/first_react_15Mar/my_first_app/src/index.js
+++ React_basics/first_react_15Mar/my_first_app/src/index.js
@@ -4,8 +4,32 @@ import ReactDom from 'react-dom';
 // stateless functional component
 // always return JSX
 
+// --- JSX RULES ---
+// return single element
+// div/ section/ article or Fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
+
 function Greeting() {
-  return <h4> Hello, Akshay here, and this is my first react app </h4>;
+  return (
+  <header>
+    <h4> Hello, Akshay here, and this is my first react app.. </h4>
+    <ul><li><a href="#">Sample link</a></li></ul>
+    <div>
+      <Person/>
+      <Message/>
+    </div>
+  </header>
+  );
 }
+
+const Person = ()=> <h1>Akshay Sahu</h1>
+const Message = ()=> <p>Heyy there, I am learning React right now...</p>
+//----------Another method------------//
+// const Greeting = ()=> {
+//   return React.createElement('h1', {}, 'Hello there, this also works')
+// }
 
 ReactDom.render(<Greeting />  , document.getElementById('root'));
