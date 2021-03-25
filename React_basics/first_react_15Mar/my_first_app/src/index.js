e20d0668dbbb58080716e6a16e8fbff0887444ee
Akshay Sahu <akki5233@gmail.com>
2021-03-25 22:11:33 +0530

    Making a section/component (Booklist) using react

--- React_basics/first_react_15Mar/my_first_app/src/index.js
+++ React_basics/first_react_15Mar/my_first_app/src/index.js
@@ -12,24 +12,55 @@ import ReactDom from 'react-dom';
 // close every element
 // formatting
 
-function Greeting() {
-  return (
-  <header>
-    <h4> Hello, Akshay here, and this is my first react app.. </h4>
-    <ul><li><a href="#">Sample link</a></li></ul>
-    <div>
-      <Person/>
-      <Message/>
-    </div>
-  </header>
-  );
-}
 
-const Person = ()=> <h1>Akshay Sahu</h1>
-const Message = ()=> <p>Heyy there, I am learning React right now...</p>
+//----Testing components---//
+
+// function Greeting() {
+//   return (
+//   <header>
+//     <h4> Hello, Akshay here, and this is my first react app.. </h4>
+//     <ul><li><a href="#">Sample link</a></li></ul>
+//     <div>
+//       <Person/>
+//       <Message/>
+//     </div>
+//   </header>
+//   );
+// }
+
+// const Person = ()=> <h1>AKSHAY SAHU</h1>
+// const Message = ()=> <p>Heyy there, I am learning React right now...</p>
+
 //----------Another method------------//
 // const Greeting = ()=> {
 //   return React.createElement('h1', {}, 'Hello there, this also works')
 // }
 
-ReactDom.render(<Greeting />  , document.getElementById('root'));
+// ReactDom.render(<Greeting />  , document.getElementById('root'));
+
+function BookList() {
+  return (
+    <section>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+
+    </section>
+  );
+}
+
+const Book = ()=> {
+  return <article>
+    <Image/>
+    <Title/>
+    <Author/>
+  </article>
+}
+const Image = ()=> <img src="https://images-na.ssl-images-amazon.com/images/I/518saTcnTvL._SX330_BO1,204,203,200_.jpg" alt="Book"/>
+
+const Title = ()=> <h1>A Brief History of Modern India (2019-2020 Edition) by Spectrum Books</h1>
+
+const Author = ()=> <h4>Rajiv Ahir</h4>
+
+ReactDom.render(<BookList/>, document.getElementById('root'));
