ff7a248914618538a3f05d250ec419e84380dedf
Akshay Sahu <akki5233@gmail.com>
2021-04-01 00:57:21 +0530

    Using props in different ways

--- React_basics/first_react_15Mar/my_first_app/src/index.js
+++ React_basics/first_react_15Mar/my_first_app/src/index.js
@@ -95,36 +95,75 @@ import './index.css';
 //   );
 // }
 
-// ---------------------------------------------------------------------------//
+// --------------------------------Making the fields Dynamic-------------------------------------------//
+
+// function BookList() {
+//   return (
+//     <section className='booklist'>
+//       <Book role="developer"/>
+//       <Book myname="Akshay"/>
+
+//     </section>
+//   );
+// }
+
+// const img = "https://images-na.ssl-images-amazon.com/images/I/518saTcnTvL._SX330_BO1,204,203,200_.jpg";
+// const title = 'A Brief History of Modern India';
+// const author = "Rajiv Ahir";
+
+// const Book = (props) => {
+//   console.log(props)
+//   return (
+//     <article className='book'>
+
+//       <img src={img} alt="Book" />
+//       <h1>{title} </h1>
+//       <h4 >{author}</h4>
+//       {/* for testing props */}
+//       <h4>{props.role}</h4>  
+//       <h4>{props.myname}</h4> 
+//     </article>
+
+//   );
+// }
+// --------------------------------Making the fields Dynamic 2-------------------------------------------//
+
+const book1 = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/518saTcnTvL._SX330_BO1,204,203,200_.jpg",
+  title: 'A Brief History of Modern India',
+  author: "Rajiv Ahir"
+}
+
+const book2 = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/51NHKZC1a0L._SX324_BO1,204,203,200_.jpg",
+  title: "Grandma's Bag of Stories",
+  author: "Sudha Murty"
+}
+
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book role="developer"/>
-      <Book />
+      <Book img = {book1.img} title = {book1.title} author = {book1.author} ></Book>
+      <Book img={book2.img} title={book2.title} author={book2.author} ></Book>
 
     </section>
   );
 }
 
-const img = "https://images-na.ssl-images-amazon.com/images/I/518saTcnTvL._SX330_BO1,204,203,200_.jpg";
-const title = 'A Brief History of Modern India';
-const author = "Rajiv Ahir";
 
-const Book = (props) => {
-  console.log(props)
+const Book = ({img, title, author}) => {
+  // console.log(props)
   return (
     <article className='book'>
 
       <img src={img} alt="Book" />
       <h1>{title} </h1>
       <h4 >{author}</h4>
-      <h4>{props.role}</h4>  // for testing props
 
     </article>
 
   );
 }
-
 
 ReactDom.render(<BookList/>, document.getElementById('root'));
