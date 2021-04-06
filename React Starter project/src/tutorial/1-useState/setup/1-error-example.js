f86cbecbd69ffd63fbe17f04d5aa537dd7297e66
Akshay Sahu <akki5233@gmail.com>
2021-04-06 22:49:41 +0530

    Preserve state and trigger re-render

--- React Starter project/src/tutorial/1-useState/setup/1-error-example.js
+++ React Starter project/src/tutorial/1-useState/setup/1-error-example.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const ErrorExample = () => {
+  let title = "Random Title";
+  const handleClick = ()=> {
+    console.log(title)
+    title = "Hello People"
+  };
   return <React.Fragment>
-    <h2>Random Title</h2>
+    <h2>{title}</h2>
+    <button type="button" className="btn" onClick={handleClick}>Change Title</button>
   </React.Fragment>
 };
 
