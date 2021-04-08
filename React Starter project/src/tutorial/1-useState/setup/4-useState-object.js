8fa0b4f3b8d7a4b3f0e37f6a099207b395bb948e
Akshay Sahu <akki5233@gmail.com>
2021-04-09 00:09:36 +0530

    UseState with Object

--- React Starter project/src/tutorial/1-useState/setup/4-useState-object.js
+++ React Starter project/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,45 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+
+  let [person, setPerson] = useState({
+    name:"Akshay",
+    age:28,
+    message:"useState with an Object"
+  });
+
+  function changeMsg(){
+    if (person.message !== "Message Changed"){
+      setPerson({ ...person, message: "Message Changed" })
+    } else {
+      setPerson({ ...person, message: "useState with an Object" })
+    }
+    
+      }
+  function changeName() {
+    if (person.name !== "Axxy") {
+      setPerson({ ...person, name: "Axxy" })
+    } else {
+      setPerson({ ...person, name: "Akshay" })
+    }
+
+  }
+  function changeAge() {
+    if (person.age !== 29) {
+      setPerson({ ...person, age: 29 })
+    } else {
+      setPerson({ ...person, age: 28 })
+    }
+
+  }
+
+  return <>
+  <br/><br/>
+    <h3 className="item">{person.name} <button onClick={()=>changeName()}>Change</button></h3>
+    <h3 className="item">{person.age} <button onClick={() => changeAge()}>Change</button></h3>
+    <h3 className="item">{person.message} <button onClick={() => changeMsg()}>Change</button></h3>
+    <button className="btn" onClick={()=> changeMsg()}>Change Message</button>
+  </>;
 };
 
 export default UseStateObject;
