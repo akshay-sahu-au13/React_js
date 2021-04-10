30244fd56d0386f3a1282542c3041b3d2892602c
Akshay Sahu <akki5233@gmail.com>
2021-04-10 18:45:17 +0530

    Increase counter using useState-counter

--- React Starter project/src/tutorial/1-useState/setup/5-useState-counter.js
+++ React Starter project/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,28 @@
 import React, { useState } from 'react';
 
+
+
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [number, setNumber] = useState(0);
+  function increase(num) {
+    setNumber(num + 1)
+  }
+  function reset(num) {
+    setNumber(0)
+  }
+  function decrease(num) {
+    setNumber(num - 1)
+  }
+  return <>
+    <section style={{ margin: '30px auto' }}>
+      <h2 >Counter</h2>
+      <h1>{number}</h1>
+      <button className="btn" onClick={()=>increase(number)}>Increase</button>
+      <button className="btn" onClick={() => reset(number)}>Reset</button>
+      <button className="btn" onClick={() => decrease(number)}>Decrease</button>
+    </section>
+  
+  </>;
 };
 
 export default UseStateCounter;
