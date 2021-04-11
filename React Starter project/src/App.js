55cf78ccab0e04e001bbdc6ee0c892de79668cfb
Akshay Sahu <akki5233@gmail.com>
2021-04-11 17:39:19 +0530

    useEffect basics

--- React Starter project/src/App.js
+++ React Starter project/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Err_eg from '../src/tutorial/1-useState/setup/1-error-example';
-import Setup from './tutorial/1-useState/setup/5-useState-counter';
+import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics';
 function App() {
   return (
     <div className='container'>
--- React Starter project/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ React Starter project/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,23 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+
+
+  let [value, setValue] = useState(0);
+
+  useEffect(()=> {
+    console.log("UseEffect");
+    document.title = `Notifications (${value})`;
+
+  }); // If you leave the dependency array empty then the useeffect will only work for 1st render
+
+
+  return <>
+    <section>
+      <h2>New notifications: {value}</h2>
+      <button className="btn" onClick={()=>setValue(value+1)}>Increase</button>
+    </section>
+  </>;
 };
 
 export default UseEffectBasics;
