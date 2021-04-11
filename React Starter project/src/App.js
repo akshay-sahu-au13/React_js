2797aa294b25fb416ff75f559f944ab711351bf3
Akshay Sahu <akki5233@gmail.com>
2021-04-11 21:05:10 +0530

    Conditional rendering

--- React Starter project/src/App.js
+++ React Starter project/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Err_eg from '../src/tutorial/1-useState/setup/1-error-example';
-import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
+import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns';
 function App() {
   return (
     <div className='container'>
--- React Starter project/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ React Starter project/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,48 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  let [user, setUser] = useState('User');
+  let [isLoading, setIsLoading] = useState(true);
+  let [isError, setIsError] = useState(false);
+
+  useEffect(()=> {
+    fetch(url).then(res=> {
+      if (res.status >= 200 && res.status < 300){
+        return res.json()
+      }
+      setIsLoading(false);
+      setIsError(true);
+      throw new Error(res.statusText);
+    }).then(user=> {
+      const {login} = user;
+      setIsLoading(false);
+      setUser(login);
+    }).catch(e=>{
+      console.log(e)
+    })
+  },[])
+
+  if (isLoading){
+    return <>
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    </>
+  }
+
+  if (isError){
+    return <>
+      <div>
+        <h1>Error...</h1>
+      </div>
+    </>
+  }
+
+  return <>
+    <div>
+      <h1>{user}</h1>
+    </div>
+  </>;
 };
 
 export default MultipleReturns;
