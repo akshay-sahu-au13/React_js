c2486e7d2d71018bd7f8cdb35643d442daaa1722
Akshay Sahu <akki5233@gmail.com>
2021-04-11 18:33:37 +0530

    Made Github profiles page for 30 users using useSate, useEffect and api

--- React Starter project/src/App.js
+++ React Starter project/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Err_eg from '../src/tutorial/1-useState/setup/1-error-example';
-import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup';
+import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial - First Project</h2>
+      <h2 style={{padding:"3rem"}}>Advanced Tutorial - First Project</h2>
       {/* <Err_eg/> */}
       <Setup/>
     </div>
