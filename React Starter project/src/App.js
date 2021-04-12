8c8affae3b061250e2991aee17cd7b8eb15cb4eb
Akshay Sahu <akki5233@gmail.com>
2021-04-12 23:26:20 +0530

    React show hide toggle

--- React Starter project/src/App.js
+++ React Starter project/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Err_eg from '../src/tutorial/1-useState/setup/1-error-example';
-import Setup from './tutorial/3-conditional-rendering/setup/2-short-circuit';
+import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide';
 function App() {
   return (
     <div className='container'>
--- React Starter project/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ React Starter project/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,35 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+
+  const [show, setShow] = useState(false);
+
+  return <>
+    <button className="btn" onClick={()=> setShow(!show)}>show / hide</button>
+    <div style={{margin : '3rem'}}>
+     {show && <Item></Item>}
+    </div>
+  </>;
 };
+
+const Item =()=> {
+  
+  let [size, setSize] = useState(window.innerWidth);
+  const showSize = () => {
+    setSize(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', showSize);
+    return () => {
+      window.removeEventListener('resize', showSize);
+    }
+  })
+
+  return <>
+    <h1>Hello there</h1>
+    <h2>window size: {size}</h2>
+  </>
+}
 
 export default ShowHide;
