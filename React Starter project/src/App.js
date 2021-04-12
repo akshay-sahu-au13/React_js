c4d819d8ec2f2d06820e4557ee1ab5257a887331
Akshay Sahu <akki5233@gmail.com>
2021-04-13 00:55:41 +0530

    Forms - Handling controlled inputs

--- React Starter project/src/App.js
+++ React Starter project/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Err_eg from '../src/tutorial/1-useState/setup/1-error-example';
-import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide';
+import Setup from './tutorial/4-forms/setup/1-controlled-inputs';
 function App() {
   return (
     <div className='container'>
--- React Starter project/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ React Starter project/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,34 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    let firstName = e.target.firstName.value;
+    let email = e.target.email.value;
+    console.log(firstName,email)
+
+  }
+
+  return <>
+    <form className="form" onSubmit={(e)=>handleSubmit(e)}>
+      <div className="form-control">
+        <label htmlFor="firstName">First Name : </label>
+        <input type="text" id="firstName" name="firstName" value={firstName} onChange={(e)=>setFirstName(e.target.value)} />
+      </div>
+
+      <div className="form-control">
+        <label htmlFor="email">Email : </label>
+        <input type="text" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+
+      <button type="submit">Submit</button>
+
+    </form>
+  </>;
 };
 
 export default ControlledInputs;
