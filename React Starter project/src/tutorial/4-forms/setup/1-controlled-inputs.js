e85e4fa997ab8216464f5a10f16f47bf7fdf96c2
Akshay Sahu <akki5233@gmail.com>
2021-04-13 01:10:54 +0530

    Forms - Handling controlled inputs

--- React Starter project/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ React Starter project/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -9,41 +9,45 @@ const ControlledInputs = () => {
 
   const [firstName, setFirstName] = useState("");
   const [email, setEmail] = useState("");
-  const [people, setPeople] = useState([{firstName:"", email:""}])
+  const [people, setPeople] = useState([{firstName:"", email:""}]);
+
   const handleSubmit = (e)=> {
     e.preventDefault();
-    let firstName = e.target.firstName.value;
-    let email = e.target.email.value;
-    console.log(firstName,email)
-    const person = {firstName,email};
-    setPeople([...people, person]);
-    setFirstName("");
-    setEmail("");
+    // let firstName = e.target.firstName.value;
+    // let email = e.target.email.value;
+    // console.log(firstName,email)
+    if (email && firstName){
+
+      const person = { id: new Date().getTime().toString(),firstName,email};
+      setPeople([...people, person]);
+      setFirstName("");
+      setEmail("");
+    }
 
   }
 
   return <>
-    <form className="form" onSubmit={(e)=>handleSubmit(e)}>
-      <div className="form-control">
-        <label htmlFor="firstName">First Name : </label>
-        <input type="text" id="firstName" name="firstName" value={firstName} onChange={(e)=>setFirstName(e.target.value)} />
-      </div>
-
-      <div className="form-control">
-        <label htmlFor="email">Email : </label>
-        <input type="text" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </div>
-
-      <button type="submit">Submit</button>
-
-    </form>
+    <article>
+      <form className="form" onSubmit={(e)=>handleSubmit(e)}>
+        <div className="form-control">
+          <label htmlFor="firstName">First Name : </label>
+          <input type="text" id="firstName" name="firstName" value={firstName} onChange={(e)=>setFirstName(e.target.value)} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="email">Email : </label>
+          <input type="text" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </article>
 
     {
       people.map(person=> {
+        const { id, firstName, email } = person;
         return <>
-          <div className="item">
-            <h4>{person.firstName}</h4>
-            <p>{person.email}</p>
+          <div className="item" key={id}>
+            <h4>{firstName}</h4>
+            <p>{email}</p>
           </div>
         </>
       })
