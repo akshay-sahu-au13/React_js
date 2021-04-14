f9b17617a17d2c87b3e70a764c0eaae88a4a7c17
Akshay Sahu <akki5233@gmail.com>
2021-04-15 01:52:40 +0530

    Cool useReducer hook - practiced with different actions

--- React Starter project/src/App.js
+++ React Starter project/src/App.js
@@ -4,8 +4,6 @@ import Setup from './tutorial/6-useReducer/setup';
 function App() {
   return (
     <div className='container'>
-      <h2 style={{padding:"3rem"}}>Advanced Tutorial - First Project</h2>
-      {/* <Err_eg/> */}
       <Setup/>
     </div>
   )
--- React Starter project/src/tutorial/6-useReducer/setup/index.js
+++ React Starter project/src/tutorial/6-useReducer/setup/index.js
@@ -2,24 +2,66 @@ import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
+const reducer = (state,action) => {
+  console.log(state, action);
 
+  if(action.type==="ADD_ITEM"){
+    const newPeople = [...state.people, action.payload]
+    return { ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: "Added the person item"
+    }
+  }
+
+  if (action.type === "NO_VALUE"){
+    return {
+      ...state,
+      isModalOpen:true,
+      modalContent:"Please enter the value"
+    }
+  }
+
+  if (action.type === "CLOSE_MODAL"){
+    return{
+      ...state,
+      isModalOpen:false
+    }
+  }
+  throw new Error ('No matching action type')
+};
+
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent:"Hello there"
+};
 const Index = () => {
-  const [name, setName] = useState("")
-  const [people, setPeople] = useState(data);
-  const [showModal, setShowModal] = useState(false);
+  const [name, setName] = useState("");
+  const [state, dispatch] = useReducer(reducer, defaultState);
+  // const [people, setPeople] = useState(data);
+  // const [showModal, setShowModal] = useState(false);
   const handleSubmit = (e)=> {
      e.preventDefault();
     if (name){
-      setShowModal(true);
-      setPeople([...people, {id: new Date().getTime().toString(), name}])
+      // setShowModal(true);
+      // setPeople([...people, {id: new Date().getTime().toString(), name}])
+      // setName("");\
+      const newItem = {id: new Date().getTime().toString(), name}
+      dispatch({type:"ADD_ITEM", payload:newItem})
       setName("");
+
     } else {
-      setShowModal(true);
+      // setShowModal(true);
+      dispatch({type:"NO_VALUE"})
     }
 }
 
+const closeModal = ()=> {
+  dispatch({type:"CLOSE_MODAL"});
+}
   return <>
-    {showModal && <Modal/>}
+    {state.isModalOpen && <Modal closeModal={closeModal} modalContent={state.modalContent} />}
     <form onSubmit={handleSubmit} className="form">
   <div>
     <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
@@ -27,6 +69,14 @@ const Index = () => {
   </div>
       <button type="submit">Add</button>
     </form>
+
+    {state.people.map(person=> {
+      return (
+        <div key={person.id}>
+          <h4>{person.name}</h4>
+        </div>
+      )
+    })}
   </>;
 };
 
