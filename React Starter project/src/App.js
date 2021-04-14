f60913d54eaefd969a446d625edc7b3f9d2e0f05
Akshay Sahu <akki5233@gmail.com>
2021-04-15 01:04:55 +0530

    Starting with useReducer hook

--- React Starter project/src/App.js
+++ React Starter project/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import Err_eg from '../src/tutorial/1-useState/setup/1-error-example';
-import Setup from './tutorial/5-useRef/setup/1-useRef-basics';
+import Setup from './tutorial/6-useReducer/setup';
 function App() {
   return (
     <div className='container'>
