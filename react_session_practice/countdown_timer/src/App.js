115b5ea2df32445cb2bb0140234a4d7c8318cb37
Akshay Sahu <akki5233@gmail.com>
2021-04-20 23:38:36 +0530

    Countdown timer initial commit

---
+++ react_session_practice/countdown_timer/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Message from "./Message";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to React App</h1>
+      <div className="underline"></div>
+
+      <Message message={"Hi There, How is your react classes going on?"}/> 
+
+    </div>
+  );
+}
+
+export default App;
---
+++ react_session_practice/countdown_timer/src/Message.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Message({message}) {
+    return (
+        <section>
+            <h4 style={{'text-decoration':'underline', 'margin-bottom': "1rem"}}>Message from Message Component</h4>
+            <h2>{message}</h2>
+
+        </section>   )
+}
