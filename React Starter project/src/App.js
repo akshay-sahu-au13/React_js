d3bba235ed8bb853b3062a209d1f4dd60747f6fb
Akshay Sahu <akki5233@gmail.com>
2021-04-20 20:48:07 +0530

    Making custom hooks

--- React Starter project/src/App.js
+++ React Starter project/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import Err_eg from '../src/tutorial/1-useState/setup/1-error-example';
-import Setup from './tutorial/8-useContext/setup/1-context-api';
+import Setup from './tutorial/9-custom-hooks/setup/1-fetch-example';
 function App() {
   return (
     <div className='container'>
