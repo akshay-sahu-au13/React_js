da916df4ec080d3f55995cd749df3324a248ad16
Akshay Sahu <akki5233@gmail.com>
2021-04-22 00:46:01 +0530

    proptrypes and defualt props1

--- React Starter project/src/tutorial/10-prop-types/setup/Product.js
+++ React Starter project/src/tutorial/10-prop-types/setup/Product.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import defaultImg from '../../../assets/default-image.jpeg';
 
 const Product = ({image, name, price}) => {
   return <article className='product'>
-    {image?<img src={image.url} alt={name} />: "Img not available"}
+    <img src={image.url} alt={name} />
       <h4>{name}</h4>
-      <p>{price?price:"Not available"}</p>
+      <p>{price}</p>
     
 
   </article>;
+};
+
+Product.propTypes = {
+  image:PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired
+};
+
+Product.defaultProps = {
+  name: 'defualt name',
+  price: 3.99,
+  image: defaultImg,
 };
 
 export default Product;
