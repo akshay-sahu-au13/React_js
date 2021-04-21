e0caa44bf842c06f8e4ca40eeab157fcd157fdb3
Akshay Sahu <akki5233@gmail.com>
2021-04-21 19:57:57 +0530

    Basic form and css done, adding functionality now

--- react_session_practice/login-form/src/App.js
+++ react_session_practice/login-form/src/App.js
@@ -1,11 +1,33 @@
-
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (<>
-    
-  </>
-  );
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      name: null,
+      age: null,
+      email: null,
+      error: false
+    }
+  }
+
+  render(){
+    return <>
+      <div className="container">
+        <form  className="form">
+
+          <div className="form-grp"><label htmlFor="name">Name: <input type="text" id="name" /></label></div>
+          <div className="form-grp"><label htmlFor="age">Age: <input type="number" id="age" /></label></div>
+          <div className="form-grp"><label htmlFor="email">Email: <input type="text" id="email"/> </label></div>
+          <button type="submit">Login</button>
+        </form>
+        <div className="error">
+          {}
+        </div>
+      </div>
+
+    </>
+  }
 }
 
-export default App;
