b3e23adbbafe1c27a4aba421dce71a9d418d9684
Akshay Sahu <akki5233@gmail.com>
2021-04-21 23:58:45 +0530

    starting prop types

--- React Starter project/src/App.js
+++ React Starter project/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import Err_eg from '../src/tutorial/1-useState/setup/1-error-example';
-import Setup from './tutorial/9-custom-hooks/setup/1-fetch-example';
+import Setup from './tutorial/10-prop-types/setup';
 function App() {
   return (
     <div className='container'>
