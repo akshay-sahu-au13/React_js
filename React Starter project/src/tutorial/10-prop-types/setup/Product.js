effe85205bba318a794ac5b57a2ca1be1c4107e9
Akshay Sahu <akki5233@gmail.com>
2021-04-22 00:34:59 +0530

    xyz?

--- React Starter project/src/tutorial/10-prop-types/setup/Product.js
+++ React Starter project/src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({image, name, price}) => {
+  return <article className='product'>
+    {image?<img src={image.url} alt={name} />: "Img not available"}
+      <h4>{name}</h4>
+      <p>{price?price:"Not available"}</p>
+    
+
+  </article>;
 };
 
 export default Product;
