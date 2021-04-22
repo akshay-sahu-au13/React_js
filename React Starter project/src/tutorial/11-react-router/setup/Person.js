7690a3939eab985e80191e35180d34f98a2eeabb
Akshay Sahu <akki5233@gmail.com>
2021-04-22 23:42:27 +0530

    Added dynamic person page

--- React Starter project/src/tutorial/11-react-router/setup/Person.js
+++ React Starter project/src/tutorial/11-react-router/setup/Person.js
@@ -2,9 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { data } from '../../../data';
 import { Link, useParams } from 'react-router-dom';
 const Person = () => {
+  const {id} = useParams();
+  const Name = data.filter(item => parseInt(id) === item.id)
+  const {name} = Name[0]
+  console.log(name)
   return (
     <div>
-      <h2>person</h2>
+      <h1>
+    {name}
+      </h1>
+      <button className="btn"><Link to="/people">Back to people</Link></button>
     </div>
   );
 };
