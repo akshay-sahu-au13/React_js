169faeee21c594c64f90b331299cd2623ae31043
Akshay Sahu <akki5233@gmail.com>
2021-04-22 22:39:25 +0530

    React Router

--- React Starter project/src/App.js
+++ React Starter project/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import Err_eg from '../src/tutorial/1-useState/setup/1-error-example';
-import Setup from './tutorial/10-prop-types/setup';
+import Setup from './tutorial/11-react-router/setup';
 function App() {
   return (
     <div className='container'>
