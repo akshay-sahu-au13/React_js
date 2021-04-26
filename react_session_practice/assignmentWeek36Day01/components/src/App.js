bd81d983474fba70e76c640dfd161ae56851179d
Akshay Sahu <akki5233@gmail.com>
2021-04-26 23:46:23 +0530

    Added some styling

--- react_session_practice/assignmentWeek36Day01/components/src/App.js
+++ react_session_practice/assignmentWeek36Day01/components/src/App.js
@@ -4,7 +4,7 @@ import ClassComp from './Class';
 function App() {
   return (
     <>
-      <div className="container">
+      <div className="container1">
         <h1>This is a functional component</h1>
         <img src="https://images.unsplash.com/photo-1580927752452-89d86da3fa0a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y29kaW5nfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="functional comp" height="300"/>
       </div>
--- react_session_practice/assignmentWeek36Day01/components/src/Class.js
+++ react_session_practice/assignmentWeek36Day01/components/src/Class.js
@@ -8,7 +8,7 @@ class ClassComp extends React.Component {
 
     render() {
         return <>
-            <div className="container">
+            <div className="container2">
                 <h1>This is a Class based Component</h1>
                 <img src="https://images.unsplash.com/photo-1542241647-9cbbada2b309?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=749&q=80" alt="classCompimg" height="300"/>
             </div>
