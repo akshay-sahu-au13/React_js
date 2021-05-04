e17f9e720fcacb2fc1b4fb2722e280068317926b
Akshay Sahu <akki5233@gmail.com>
2021-05-04 18:24:46 +0530

    Todo App - Added CSS properties

---
+++ react_session_practice/CCWeek37Day02/todo-list/src/components/addItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AddItem() {
+    return (
+        <div>
+            <input type="text" name="todoItem" value={} onChange={} />
+            <button className="btn"> Add-Item</button>
+        </div>
+    )
+}
