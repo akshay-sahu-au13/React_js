f3e169004a0af06aa5974d3596349b4738d3ea30
Akshay Sahu <akki5233@gmail.com>
2021-05-07 01:01:11 +0530

    Basic CSS Done

---
+++ react_session_practice/assn_week37_day05/router/src/containers/App.js
@@ -0,0 +1,29 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Route, BrowserRouter, Switch} from 'react-router-dom'
+
+function App() {
+  return (<>
+    <h1>App</h1>
+
+    <div className="container">
+      <nav className="navbar">
+
+      </nav>
+
+      <section>
+        <div className="about">
+
+        </div>
+
+        <div className="exp">
+          
+        </div>
+      </section>
+    </div>
+
+    </>
+  );
+}
+
+export default App;
