d22b7aad3b67083f38564591b48dd92ae8cc8580
Akshay Sahu <akki5233@gmail.com>
2021-05-09 00:35:59 +0530

    Making the Login page

--- react_session_practice/assn_week37_day05/router/src/containers/Login/index.js
+++ react_session_practice/assn_week37_day05/router/src/containers/Login/index.js
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React, { useState, useEffect} from 'react';
 function Login() {
+
+    const [ value, setValue ] = useState({});
+
+    const changeHandler = (e) => {
+        setValue({
+            [e.target.name] : e.target.value
+        })
+    }
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="login">
+            <form method="post" >
+
+                <div className="form-grp">
+                    <label htmlFor="name">
+                        Name: <input type="text" name="name" id="name" value={value.name} onChange={changeHandler}/>
+                    </label>
+                </div>
+
+            </form>
         </div>
     )
 }
