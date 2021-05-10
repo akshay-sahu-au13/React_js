936377cf15433450ccadd3695c7bb42df0c22121
Akshay Sahu <akki5233@gmail.com>
2021-05-10 23:20:26 +0530

    Adding react-royer-dom

--- react_session_practice/cc_week38_day01/src/container/App/App.js
+++ react_session_practice/cc_week38_day01/src/container/App/App.js
@@ -1,13 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
+import {Route, BrowserRouter, Switch} from 'react-router-dom';
 // import Button from '../../Button';
 import Posts from '../Posts';
 
 function App() {
   return (
     <>
-    <h1>App</h1>
-    <Posts />
+    <BrowserRouter>
+
+      <h1>App</h1>
+      <Switch>
+        <Route exact path="/posts" component={Posts} />
+      </Switch>
+    </BrowserRouter>
     </>
   );
 }
