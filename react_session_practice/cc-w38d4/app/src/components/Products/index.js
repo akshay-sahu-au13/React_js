a08c36fd91788e8e3e58d8987ec597d2033c2914
Akshay Sahu <akki5233@gmail.com>
2021-05-14 23:55:25 +0530

    Adding product pages

---
+++ react_session_practice/cc-w38d4/app/src/components/Products/index.js
@@ -0,0 +1,22 @@
+import { useState, useEffect, useCallback } from 'react';
+import { connect } from 'react-redux';
+
+
+const Products = (props) => {
+    console.log("Product props: ",props);
+
+    const fecthData
+
+    return <>
+        <h1>Products</h1>
+    
+    </>
+}
+
+const mapStateToProps = (state) => {
+    return {
+        products:state.products
+    }
+}
+
+export default Products;
--- react_session_practice/cc-w38d4/app/src/containers/App/App.js
+++ react_session_practice/cc-w38d4/app/src/containers/App/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {connect} from 'react-redux';
+import Products from '../../components/Products';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <h1>App</h1>
+      <Switch>
+        <Route exact path="/products" component={Products}/>
+
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
+
+
 
 export default App;
