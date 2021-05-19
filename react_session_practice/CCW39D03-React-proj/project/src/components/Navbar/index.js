627cff8eba0b166b1610f0c5e59e86e14ed5c69b
Akshay Sahu <akki5233@gmail.com>
2021-05-19 21:00:28 +0530

    Basic folder structure

---
+++ react_session_practice/CCW39D03-React-proj/project/src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './navbar.css';
+function Navbar() {
+    return (
+        <div>
+            <h2>Navbar</h2>
+        </div>
+    )
+}
+
+export default Navbar;
