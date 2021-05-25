11f289783e59b2724193c1fd219c85dd3cb71c16
Akshay Sahu <akki5233@gmail.com>
2021-05-26 00:05:43 +0530

    Profile details added

--- react_session_practice/Assn-W40-d01-project d03/project_d03/src/containers/Profile/index.js
+++ react_session_practice/Assn-W40-d01-project d03/project_d03/src/containers/Profile/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import './Profile.css';
-function Profile() {
-    return (
-        <div className="profile">
-            <div className="top">
-                <h1>This is the Profile Page</h1>
+import { Redirect } from 'react-router-dom';
+import {useSelector} from 'react-redux';
+import PATHS from '../../config/webPaths';
+
+function Profile(props) {
+    console.log("Profile/props => ",props)
+    const auth = useSelector(state => state.auth)
+    const profileData = useSelector(state => state.profile);
+    console.log("Profile Data: ", profileData)
+
+    return (<>
+        {
+            auth ? 
+            <>
+            <div className="profile">
+                <div className="top">
+                    <img src={profileData.imageUrl} alt={`${profileData.firstName}'s dp`} />
+                    <h1>Welcome, {profileData.firstName} {profileData.lastName}</h1>
+                </div>
+                <div className="bottom">
+                    <h3>User Info here!</h3>
+                </div>
             </div>
-            <div className="bottom">
-                <h3>User Info here!</h3>
-            </div>
-        </div>
+            </> 
+            : <Redirect to={PATHS.HOME} />
+            
+        }
+        </>
     )
 }
 
