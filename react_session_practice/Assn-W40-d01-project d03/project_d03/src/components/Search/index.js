57fd879e09aff7581fda45c4d1612c04cf9c94e3
Akshay Sahu <akki5233@gmail.com>
2021-05-26 02:24:14 +0530

    search function implemented

--- react_session_practice/Assn-W40-d01-project d03/project_d03/src/components/Search/index.js
+++ react_session_practice/Assn-W40-d01-project d03/project_d03/src/components/Search/index.js
@@ -1,9 +1,22 @@
-import React from 'react'
-import './search.css'
+import { useState } from 'react';
+import './search.css';
+import { useDispatch } from 'react-redux';
+import videoActions  from '../../actions/videoActions';
+
 function SearchBar() {
+
+    const dispatch = useDispatch();
+
+    const [search, setSearch] = useState("");
+    const handleChange = (e) => {
+        setSearch(e.target.value);
+        dispatch(videoActions.list(e.target.value))
+
+    }
+
     return (
         <div className="search-bar">
-            <input type="text" name="search" placeholder="Search videos here"/>
+            <input type="text" name="search" value={search} placeholder="Search videos here" onChange={handleChange}/>
             <span id="search-icon" class="material-icons">
                 search
             </span>
