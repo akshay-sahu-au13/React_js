2bbc6970f4e6c277a844f096cd5d2c9e113eac8c
Akshay Sahu <akki5233@gmail.com>
2021-05-25 20:18:23 +0530

    React project day04

---
+++ react_session_practice/cc-w40d02/cc/src/actionTypes/index.js
@@ -0,0 +1,22 @@
+export const searchTypes = {
+    search: "search/SEARCH_INPUT"
+}
+
+export const videoTypes = {
+    list: "video/VIDEO_LIST"
+}
+
+export const profileTypes = {
+    details: "profile/PROFILE_DETAILS"
+}
+
+export const authTypes = {
+    login: "auth/USER_LOGIN",
+    logout: "auth/USER_LOGOUT"
+}
+
+export const loaderTypes = {
+    start: "loader/START_LOADING",
+    stop: "loader/STOP_LOADING"
+}
+
